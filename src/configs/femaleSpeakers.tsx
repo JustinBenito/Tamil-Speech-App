4a0cd02c6c3c811afbe52bc2aace9dab954a0d69
JustinBenito <justinbenito1974@gmail.com>
2024-03-10 16:04:08 +0530

    TTS speach Viseme

--- src/configs/femaleSpeakers.tsx
+++ src/configs/femaleSpeakers.tsx
@@ -1,7 +1,5 @@
 export const FemaleSpeakers = [
-    {value:"en-GB-SoniaNeural", label:"en-GB-SoniaNeural"},
-    {value:"en-US-JennyNeural", label:"en-US-JennyNeural"},
-    {value:"zh-CN-XiaoxiaoNeural", label:"zh-CN-XiaoxiaoNeural"}
+    {value:"ta-IN-PallaviNeural", label:"Tamil"},
 
 ]
 
