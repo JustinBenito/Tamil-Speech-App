f19a70c0607bcb5072750a882c3dda309989f137
JustinBenito <justinbenito1974@gmail.com>
2024-03-18 15:37:07 +0530

    update

--- asr.py
+++ asr.py
@@ -14,38 +14,37 @@
 #     print(e)
 
 
-# login user
+# login user 
+import requests
 
-# import requests
-
-# headersList = {}
-# payload = {
-# 'email': "justinbenito2213005@ssn.edu.in",
-# 'password': "Justin@2005",
-# }
-# try:
-#     response = requests.post('https://asr.iitm.ac.in/api/accounts/login/',
-#     data=payload, headers=headersList)
-#     print(response.json())
-# except Exception as e:
-#     print(e)
+headersList = {}
+payload = {
+'email': "justinbenito2213005@ssn.edu.in",
+'password': "Justin@2005",
+}
+try:
+    response = requests.post('https://asr.iitm.ac.in/api/accounts/login/',
+    data=payload, headers=headersList)
+    print(response.json())
+except Exception as e:
+    print(e)
 
 
 # asr code
 
-import requests
-headersList = {
-"Authorization": "Token 1b0fedcb19b28f1500918dea8847ad3f2864ab5b79221c8c98734218ecd57404"
-}
-files = {
-'file': open('tamil.mp3', 'rb'),
-'language': (None, 'tamil'),
-'vtt': (None, 'true'),
-}
+# import requests
+# headersList = {
+# "Authorization": "Token 1b0fedcb19b28f1500918dea8847ad3f2864ab5b79221c8c98734218ecd57404"
+# }
+# files = {
+# 'file': open('tamil.mp3', 'rb'),
+# 'language': (None, 'tamil'),
+# 'vtt': (None, 'true'),
+# }
 
-try:
-    response = requests.post('https://asr.iitm.ac.in/api/asr/', files=files,
-    headers=headersList)
-    print(response.json())
-except Exception as e:
-    print(e)
+# try:
+#     response = requests.post('https://asr.iitm.ac.in/api/asr/', files=files,
+#     headers=headersList)
+#     print(response.json())
+# except Exception as e:
+#     print(e)
